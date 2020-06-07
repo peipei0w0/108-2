134521922cbbd9fc55309b69791c660d36001dca
陳珮瑄 <59371107+peipei0w0@users.noreply.github.com>
2020-06-07 22:50:53 +0800

    Update LeapYear_NotOOP.py

--- 物件 py/LeapYear_NotOOP/LeapYear_NotOOP.py
+++ 物件 py/LeapYear_NotOOP/LeapYear_NotOOP.py
@@ -2,6 +2,7 @@
 ##西元年份除以4可整除，且除以100不可整除，為閏年。
 ##西元年份除以100可整除，且除以400不可整除，為平年
 ##西元年份除以400可整除，為閏年。
+
 def isLeapYear(newYear):
     if (newYear % 400 == 0) or (newYear % 100 != 0 and newYear % 4 == 0):
         return True
