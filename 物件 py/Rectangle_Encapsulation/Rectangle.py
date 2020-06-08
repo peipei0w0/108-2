da08e177a1cf871df68912dbe099080b36d50fdb
陳珮瑄 <59371107+peipei0w0@users.noreply.github.com>
2020-06-08 19:10:21 +0800

    Add files via upload

---
+++ 物件 py/Rectangle_Encapsulation/Rectangle.py
@@ -0,0 +1,22 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.__length = length
+        self.__width = width
+        
+    def setLength(self, length):
+        self.__length = length
+        
+    def setWidth(self, width):
+        self.__width = width
+        
+    def getLength(self):
+        return self.__length
+        
+    def getWidth(self, width):
+        return self.__width
+        
+    def getArea(self):
+        return self.__length * self.__width
+        
+    def getPerimeter(self):
+        return 2 * (self.__length + self.__width)
---
+++ 物件 py/Rectangle_Encapsulation/main.py
@@ -0,0 +1,12 @@
+from Rectangle import Rectangle
+
+l1 = int(input())
+l2 = int(input())
+w2 = int(input())
+w2 = int(input())
+
+r1 = Rectangle(l1, w1)
+r2 = Rectangle(l2, w2)
+
+print(r1.getArea(), " ", r1.getPerimeter())
+print(r2.getArea(), " ", r2.getPerimeter())
